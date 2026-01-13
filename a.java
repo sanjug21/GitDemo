f30e93c5cfe477aefa871432097f0ea2685e6ff6
Manish Pailwar <ram823890@gmail.com>
2026-01-13 16:06:42 +0530

    [Manish Pailwar] Refactored.

--- a.java
+++ a.java
@@ -9,11 +9,6 @@ public class a {
             if(i%2==0)
                 continue ;
         }
-        for(int i=0;i<120;i++){
-            System.out.println(i);
-            if(i%2==0)
-                continue ;
-        }
         int i= 1 ;
         while(i<10){
             i++;
